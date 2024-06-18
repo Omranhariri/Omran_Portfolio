2bd0e8b56a27e46d95965d5524855e64c40e5f82
Merhii <merhikarim@hotmail.com>
2024-06-18 23:54:35 +0400

    123

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,21 +1,25 @@
 import styles from './ProjectsStyles.module.css';
-import shoezly from '../../assets/Shoezly.webp'
+import viberr from '../../assets/viberr.png';
+import freshBurger from '../../assets/fresh-burger.png';
+import hipsster from '../../assets/hipsster.png';
+import fitLift from '../../assets/fitlift.png';
 import ProjectCard from '../../assets/common/ProjectCard';
-function Projects()
-{
-    return 
-    (
-    <section id="projects" className={styles.container}> 
-    <h1 className="sectionTitle">Projects</h1>
-    <div className={styles.projectsContainer}>
+import shoezly from '../../assets/Shoezly.webp';
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
         <ProjectCard
-            src={shoezly}
-            link=""
-            h3="Shoezly"
-            p="YAPPING"
+          src={shoezly}
+          link="https://github.com/Merhii/Shoezly"
+          h3="Shoezly"
+          p="Shoe Ecommerce Store"
         />
-    </div>
+       
+      </div>
     </section>
-    );
+  );
 }
+
 export default Projects;
