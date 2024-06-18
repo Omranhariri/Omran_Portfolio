65bcab6a79a853849a2571a3f8d583d9423bdcf4
Merhii <merhikarim@hotmail.com>
2024-06-18 18:58:42 +0400

    added css styling

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,17 +1,26 @@
 import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/hero-img.png'
-import themeIcon from '../../assets/sun.svg'
-import linkedinIcon from '../../assets/linkedin-light.svg'
-import githubIcon from '../../assets/github-light.svg'
-import CV from '../../assets/cv.pdf'
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import githubLight from '../../assets/github-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import githubDark from '../../assets/github-dark.svg';
+import CV from '../../assets/cv.pdf';
 import { useTheme } from '../../assets/common/ThemeContext';
+
 function Hero() {
-    const {theme,toggleTheme}= useTheme();
+    const { theme, toggleTheme } = useTheme();
+
+    const themeIcon = theme === 'light' ? sun : moon;
+    const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+    const githubIcon = theme === 'light' ? githubLight : githubDark;
+  
   return (
     <section id="hero" className={styles.container}>
 <div className={styles.colorModeContainer}>
     <img className={styles.hero} src={heroImg} alt="Profile Picture of Karim Merhi" />
-<img className={styles.colorMode} src={themeIcon} alt="Color mode icon" />
+<img className={styles.colorMode} src={themeIcon} alt="Color mode icon" onClick={toggleTheme}/>
 </div>
 <div>
     <h1> Karim
