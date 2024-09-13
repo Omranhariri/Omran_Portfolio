87dda93b05c84daaa2baabc005d78ea6c9d5a70b
omranhariri <omranhariri29@gmail.com>
2024-09-13 23:29:05 +0300

    added n

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()], 
-  base : '/Omran_Portfolio/'
+  base : '/Omran_Portfolio.github.io/'
 })
