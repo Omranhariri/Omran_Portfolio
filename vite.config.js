b77a96675c3a82fe1606b11497e34fbac79a92ab
omranhariri <omranhariri29@gmail.com>
2024-09-13 23:22:50 +0300

    added nanme

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()], 
-  base : '/Karim-Merhi'
+  base : '/Omran_Portfolio/'
 })
